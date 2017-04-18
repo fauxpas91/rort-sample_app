1131b79c2f8bf96c7c50e886e71aea65814ca410
Paul Couch Burden <paulcb09@gmail.com>
2017-04-18 12:39:01 +0000

    Finished adding static pages, added controller tests, added a new contact and about, actions and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def home
-      render text: "This is the Homepage"
-  end
+  # def home
+  #     render text: "This is the Homepage"
+  # end
 end
